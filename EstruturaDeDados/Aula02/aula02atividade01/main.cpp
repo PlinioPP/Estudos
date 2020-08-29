3f0bd7252bf14351b2cb79e6b15358da1ef3cea3
Plinio <pppplinio@hotmail.com>
2020-08-29 17:06:30 -0300

    20200829

---
+++ EstruturaDeDados/Aula02/aula02atividade01/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int v;
+    float vetor[10];
+
+    for (v=0;v<=10;v++);
+     cout << "Digite um valor numérico" << endl;
+     cin >> vetor[v];
+      if {vetor[v]<=0
+      }
+
+     return 0;
+}
